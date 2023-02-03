c68b0106841ce81b6099d157e1c224f0deb05521
Brishna Bakshev <b.bakshev@gmail.com>
2023-02-03 09:37:47 -0800

    good fail because the expected output shows a number but received is empty

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -1,19 +1,23 @@
 import GalacticAge from "../src/js/age";
 
-describe('GalacticAge', () => {
-  test('should create an Earth age property within the GalacticAge class', () => {
+describe("GalacticAge", () => {
+  test("should create an Earth age property within the GalacticAge class", () => {
     const myGalacticAge = new GalacticAge(25);
     expect(myGalacticAge.earthAge).toEqual(25);
   });
 
-  test('should calculate the users age in Mercury years', () => {
+  test("should calculate the users age in Mercury years", () => {
     const myGalacticAge = new GalacticAge(25);
     expect(myGalacticAge.mercuryAge).toEqual(25 / 0.24);
   });
 
-  test('should calculate the users age in Venus years', () => {
+  test("should calculate the users age in Venus years", () => {
     const myGalacticAge = new GalacticAge(25);
     expect(myGalacticAge.venusAge).toEqual(25 / 0.62);
   });
 
+  test("should calculate the users age in Mars years", () => {
+    const myGalacticAge = new GalacticAge(25);
+    expect(myGalacticAge.marsAge).toEqual(25 / 1.88);
+  });
 });
--- src/js/age.js
+++ src/js/age.js
@@ -3,8 +3,7 @@ export default class GalacticAge {
     this.earthAge = earthAge;
     this.pastAge = pastAge;
     this.futureAge = futureAge;
-    this.mercuryAge = parseFloat((earthAge / 0.24));
-    this.venusAge = parseFloat((earthAge / 0.62));
+    this.mercuryAge = parseFloat(earthAge / 0.24);
+    this.venusAge = parseFloat(earthAge / 0.62);
   }
-
 }
