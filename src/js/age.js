0624423100322fac384cc0a3952490dfad98409a
Brishna Bakshev <b.bakshev@gmail.com>
2023-02-03 09:22:27 -0800

    test passed because venus property has been declared

--- src/js/age.js
+++ src/js/age.js
@@ -1,9 +1,10 @@
 export default class GalacticAge {
-  constructor(earthAge, pastAge, futureAge, mercuryAge) {
+  constructor(earthAge, pastAge, futureAge, mercuryAge, venusAge) {
     this.earthAge = earthAge;
     this.pastAge = pastAge;
     this.futureAge = futureAge;
     this.mercuryAge = parseFloat((earthAge / 0.24));
+    this.venusAge = parseFloat((earthAge / 0.62));
   }
 
 }
