ade57aeb92e9a86985da7e203cb81ae80ba71ed1
Brishna Bakshev <b.bakshev@gmail.com>
2023-02-03 08:42:49 -0800

    test passed after defining the properties

--- src/js/age.js
+++ src/js/age.js
@@ -1,6 +1,8 @@
 export default class GalacticAge {
   constructor(earthAge, pastAge, futureAge) {
-    
+    this.earthAge = earthAge;
+    this.pastAge = pastAge;
+    this.futureAge = futureAge;
   }
 
 }
