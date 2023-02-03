c1a34466532fa2b2470023690cc5308be72b06cc
Brishna Bakshev <b.bakshev@gmail.com>
2023-02-03 09:38:58 -0800

    test passed because the property for mars has been declared

--- src/js/age.js
+++ src/js/age.js
@@ -1,9 +1,10 @@
 export default class GalacticAge {
-  constructor(earthAge, pastAge, futureAge, mercuryAge, venusAge) {
+  constructor(earthAge, pastAge, futureAge, mercuryAge, venusAge, marsAge) {
     this.earthAge = earthAge;
     this.pastAge = pastAge;
     this.futureAge = futureAge;
     this.mercuryAge = parseFloat(earthAge / 0.24);
     this.venusAge = parseFloat(earthAge / 0.62);
+    this.marsAge = parseFloat(earthAge / 1.88);
   }
 }
